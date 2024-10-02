25ba6b7f2f3b5e4ac59652bcb3c38b22faaa04e1
Amanda Benjamin <amanda.benjamin@mths.ca>
2024-10-02 18:18:15 -0400

    moved back to "thing to get loop to work and spacing"

--- DICE.java
+++ DICE.java
@@ -38,7 +38,7 @@ final class DICE {
 
     public static void main(final String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
+Scanner scanner = new Scanner(System.in);
 		Random random = new Random();
 
     	System.out.println("THIS PROGRAM SIMLUATES THE ROLLING OF A");
@@ -49,26 +49,26 @@ final class DICE {
 
 		String useranswer = "YES";
 
-		while (useranswer.equalsIgnoreCase("YES") || useranswer.equalsIgnoreCase("Y")); {
 		int[] frequency = new int[13];
-			if (true); {
-			System.out.print("HOW MANY ROLLS: ");
-			int rollamount = scanner.nextInt();
 
-				for (int counter = 0; counter < rollamount; counter++) {
-					int firstdie = random.nextInt(6) + 1;
-					int seconddie = random.nextInt(6) + 1;
-					int total = firstdie + seconddie;
-					frequency[total]++;
-				}
+		if (useranswer.equalsIgnoreCase("YES") || useranswer.equalsIgnoreCase("Y")); {
+		System.out.print("HOW MANY ROLLS: ");
+		int rollamount = scanner.nextInt();
 
-				for (int amountcounter = 0; amountcounter <=12; amountcounter++) {
-					System.out.println("TOTAL SPOTS " + amountcounter + "        NUMBER OF TIMES " + frequency[amountcounter]);
-				}
-
-				System.out.print("TRY AGAIN? ");
-				useranswer = scanner.nextLine();
+			for (int counter = 0; counter < rollamount; counter++) {
+				int firstdie = random.nextInt(6) + 1;
+				int seconddie = random.nextInt(6) + 1;
+				int total = firstdie + seconddie;
+				frequency[total]++;
 			}
-		}
+
+			for (int amountcounter = 0; amountcounter <=12; amountcounter++) {
+				System.out.println("TOTAL SPOTS" + amountcounter + "        NUMBER OF TIMES" + frequency[amountcounter]);
+			}
+
+			System.out.print("TRY AGAIN? ");
+			useranswer = scanner.nextLine();
+    	}
+		useranswer = scanner.nextLine();
 	}
 }
